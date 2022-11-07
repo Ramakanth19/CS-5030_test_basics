6f928a60f37216df56b1118549bffba9e9d2bf38
Ramakanth19 <113057466+Ramakanth19@users.noreply.github.com>
2022-11-07 16:28:42 -0600

    Test_Case_Assignment Completed Successfully

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -23,16 +23,38 @@ class todoservice{
     }
 
     add_todo(todo){
+        //const addRecord = new todoservice(todo);
+        //const arr=this.todos;
+        //console(arr.push(addRecord));
+        //const addRecord = new todoservice(this.todos);
+        //const arr = Array.from(addRecord);
+        //arr.push(todo);
+        this.todos=this.todos.todo.concat(todo);
+        console.log(this.todos);
         // Your code here
     }
+
 
     delete_todo(id){
+        this.todos = this.todos.filter(todo => todo.title !== id)
+        console.log(this.todos);
+        // Your code here
+    }
+    
+    update_todo(id, todo){
+
+        this.todos.map(todo_update => {
+            if (todo_update.title === id) {
+                todo_update.title = todo;
+                todo_update.description=todo
+            }
+            //return todo
+        })
+        console.log(this.todos);
         // Your code here
     }
 
-    update_todo(id, todo){
-        // Your code here
-    }
+    
 }
 
 
